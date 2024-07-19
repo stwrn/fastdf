89769d69454da3877ad30ce14bb2d84ac8c81a22
stwrn <stwrnstwrn@gmail.com>
2024-07-19 08:58:13 +0400

    Prepare for PyPI release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fastdf",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="your.email@example.com",
     description="A high-performance DataFrame implementation built on top of NumPy",
--- src/fastdf/__init__.py
+++ src/fastdf/__init__.py
@@ -1,4 +1,4 @@
 from .core import FastDataFrame as fdf
 
 __all__ = ['fdf']
-__version__ = '0.1.0'
+__version__ = '0.1.1'
